75cc45d633e7a7b039a3e36312adfd179be0217b
Enrico Benini <e.benini@apexnet.it>
2015-03-10 17:20:49 +0100

    Add code documentation

--- LDAPLibrary/LdapConfigRepository.cs
+++ LDAPLibrary/LdapConfigRepository.cs
@@ -101,7 +101,18 @@ namespace LDAPLibrary
         }
 
         #endregion
-
+        /// <summary>
+        /// Check the validity of parameters
+        /// Set the basic Ldap Configuration from parameters
+        /// Set the others to the standard values
+        /// </summary>
+        /// <param name="adminUser">Admin User</param>
+        /// <param name="adminMode">Library Admin Mode</param>
+        /// <param name="server">Server URL</param>
+        /// <param name="searchBaseDn">Root Search Node</param>
+        /// <param name="authType"></param>
+        /// <param name="loggerType"></param>
+        /// <param name="logPath">Path of the log file</param>
         public void BasicLdapConfig(ILdapUser adminUser, LDAPAdminMode adminMode, string server, string searchBaseDn, AuthType authType, LoggerType loggerType, string logPath)
         {
             BasicLdapConfigValidator(server, loggerType, logPath,searchBaseDn,adminUser,adminMode);
@@ -117,6 +128,16 @@ namespace LDAPLibrary
             StandardLdapInformation();
         }
 
+        /// <summary>
+        /// Check the validity of the parameters
+        /// Set addiitonal configuration form the parameters
+        /// </summary>
+        /// <param name="secureSocketLayerFlag"></param>
+        /// <param name="transportSocketLayerFlag"></param>
+        /// <param name="clientCertificateFlag"></param>
+        /// <param name="clientCertificatePath">Path of the certificate file</param>
+        /// <param name="userObjectClass">Object class that rapresent an user</param>
+        /// <param name="matchFieldUsername">Attribute that rapresent the username</param>
         public void AdditionalLdapConfig(
             bool secureSocketLayerFlag, bool transportSocketLayerFlag, bool clientCertificateFlag,
             string clientCertificatePath, string userObjectClass,
@@ -133,7 +154,7 @@ namespace LDAPLibrary
         }
 
         /// <summary>
-        ///     Set LDAP Information To standard values.
+        /// Set LDAP Information To standard values.
         /// </summary>
         private void StandardLdapInformation()
         {
@@ -146,6 +167,23 @@ namespace LDAPLibrary
             _matchFieldUsername = "cn";
         }
 
+        /// <summary>
+        /// Check the validity of parameters
+        /// 
+        /// * Server and searchBaseDn cannot be null or empty
+        /// * logPath cannot be null or empty if the loggerType is set to File
+        /// * Admin cannot be null if there's admin mode
+        /// 
+        /// Used in the BasicLdapConfig method
+        /// 
+        /// Can throw an ArgumentNullException
+        /// </summary>
+        /// <param name="server">Server URL</param>
+        /// <param name="loggerType"></param>
+        /// <param name="logPath">Path of the log file</param>
+        /// <param name="searchBaseDn">Search Root Node</param>
+        /// <param name="admin">Admin User</param>
+        /// <param name="adminMode">Library Admin Mode</param>
         private static void BasicLdapConfigValidator(string server, LoggerType loggerType, string logPath, string searchBaseDn, ILdapUser admin, LDAPAdminMode adminMode)
         {
             if (LdapParameterChecker.ParametersIsNullOrEmpty(new[] { server, searchBaseDn }) ||
@@ -154,6 +192,18 @@ namespace LDAPLibrary
                 throw new ArgumentNullException(String.Format(BasicConfigNullParametersErrorMessage));
         }
 
+        /// <summary>
+        /// Check the validity of parameters
+        /// 
+        /// * Check if clientCertificatePath, userObjectClass, matchFieldUsername are null or empty
+        /// 
+        /// Used in AdditionalLdapConfig
+        /// 
+        /// Can throw an ArgumentNullException
+        /// </summary>
+        /// <param name="clientCertificatePath"></param>
+        /// <param name="userObjectClass"></param>
+        /// <param name="matchFieldUsername"></param>
         private void AddictionalLdapConfigValidator(string clientCertificatePath, string userObjectClass,
             string matchFieldUsername)
         {
