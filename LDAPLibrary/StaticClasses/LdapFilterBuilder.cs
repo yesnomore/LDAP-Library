2382eb1fcb481aa562233744f12274c5dbf245b8
Enrico Benini <benkio89@gmail.com>
2016-02-19 09:50:10 +0100

    Add new kind of search filters in the ldap library Update the model Add comments

--- LDAPLibrary/StaticClasses/LdapFilterBuilder.cs
+++ LDAPLibrary/StaticClasses/LdapFilterBuilder.cs
@@ -5,13 +5,47 @@ using System.Runtime.CompilerServices;
 
 namespace LDAPLibrary.StaticClasses
 {
+    /// <summary>
+    /// Class that build the search filter for the search methods
+    /// </summary>
     internal static class LdapFilterBuilder
     {
-        private const string SearchFilterTemplate = "(&(objectClass={0})({1}={2}))";
+        private const string SearchFilterTemplateObjectClassAndFieldMatch = "(&(objectClass={0})({1}={2}))";
+        private const string SearchFilterTemplateObjectClassMatch = "(objectClass={0})";
+        private const string SearchFilterTemplateAllMatch = "(objectClass=*)";
 
+        /// <summary>
+        /// Build a filter that fetch only the nodes with:
+        /// The specified object class
+        /// The match between the second parameter and the third.
+        /// </summary>
+        /// <param name="objectClass">Object class value to match</param>
+        /// <param name="fieldUsername">Fieldname to match</param>
+        /// <param name="user">field value to match</param>
+        /// <returns></returns>
         public static string GetSearchFilter(string objectClass, string fieldUsername, string user)
         {
-            return String.Format(SearchFilterTemplate, objectClass, fieldUsername, user);
+            return String.Format(SearchFilterTemplateObjectClassAndFieldMatch, objectClass, fieldUsername, user);
+        }
+
+        /// <summary>
+        /// Build a filter that fetch only the nodes with:
+        /// The specified object class
+        /// </summary>
+        /// <param name="objectClass">Object class value to match</param>
+        /// <returns></returns>
+        public static string GetSearchFilter(string objectClass)
+        {
+            return String.Format(SearchFilterTemplateObjectClassMatch, objectClass);
+        }
+
+        /// <summary>
+        /// Fetch all the nodes in the base one. NO FILTER.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetSearchFilter()
+        {
+            return String.Format(SearchFilterTemplateAllMatch);
         }
     }
 }
