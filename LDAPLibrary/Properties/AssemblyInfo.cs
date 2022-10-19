6a5bbfd299044d4d297462a32fe2cb8ddd6be484
HP15-LUCA\Luca Romanelli <romanelli@lenis.tech>
2022-10-19 17:46:37 +0200

    Aggiunta file sharedAssemblyInfo a solutions e progetti

--- LDAPLibrary/Properties/AssemblyInfo.cs
+++ LDAPLibrary/Properties/AssemblyInfo.cs
@@ -13,7 +13,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LDAPLibrary")]
-[assembly: AssemblyCopyright("Enrico Benini Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -26,17 +25,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 
 [assembly: Guid("7e28125d-1450-40db-a642-4aeeb17ace05")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("3.5.0")]
-[assembly: AssemblyFileVersion("3.5.0")]
---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCopyright("Enrico Benini Copyright ©  2013")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.5.0")]
+[assembly: AssemblyFileVersion("3.5.0")]
